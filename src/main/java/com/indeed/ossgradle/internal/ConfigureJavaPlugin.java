e581e13b362487dbfbad9e2e8702d02a0b02fed6
Michael Morrison <517502+mmorrisontx@users.noreply.github.com>
2022-01-07 15:32:40 -0600

    Force charset for javadoc construction

--- src/main/java/com/indeed/ossgradle/internal/ConfigureJavaPlugin.java
+++ src/main/java/com/indeed/ossgradle/internal/ConfigureJavaPlugin.java
@@ -3,6 +3,7 @@ package com.indeed.ossgradle.internal;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.tasks.compile.JavaCompile;
+import org.gradle.api.tasks.javadoc.Javadoc;
 
 public class ConfigureJavaPlugin implements Plugin<Project> {
     @Override
@@ -13,6 +14,12 @@ public class ConfigureJavaPlugin implements Plugin<Project> {
                         task -> {
                             task.setSourceCompatibility("1.8");
                             task.setTargetCompatibility("1.8");
+                            task.getOptions().setEncoding("UTF-8");
+                        });
+        project.getTasks()
+                .withType(Javadoc.class)
+                .configureEach(
+                        task -> {
                             task.getOptions().setEncoding("UTF-8");
                         });
     }
